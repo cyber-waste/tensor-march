5117e890655c0740c8f8a45b6b1c4e2a198767ae
VladGritsun <vlg92@mail.ru>
2013-10-22 15:59:23 +0300

    small addition

--- incubator/tensor-test/src/factorization.py
+++ incubator/tensor-test/src/factorization.py
@@ -3,4 +3,23 @@ Created on Oct 22, 2013
 
 @author: vlg
 '''
-print "Hello"
+
+import random
+
+def gen_tensor(N):
+    def gen_matrix():
+        return [[ random.randint(0,1000) for _ in range(N)] for _ in range(N)]
+    
+    return [ gen_matrix() for _ in range(N)]
+
+def print_tensor(tensor):
+    N = len(tensor)
+    for i in range(N) :
+        for j in range(N) :
+             print tensor[i][j]
+        print
+
+def factorize(tensor):
+    pass
+
+print_tensor(gen_tensor(2))
